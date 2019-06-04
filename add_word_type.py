1ed20f31bc47982ede0eea1791e20aea8dfa2c28
nomadlife15u <aaeacd@gmail.com>
2019-06-04 12:42:48 +0900

    update sql script

--- add_word_type.py
+++ add_word_type.py
@@ -1,24 +1,50 @@
-import sqlite3, sys
+import sqlite3, sys, csv
 
-if len(sys.argv) > 1:
-    # print(sys.argv[1])
-    word = sys.argv[1]
+# if len(sys.argv) > 1:
+#     # print(sys.argv[1])
+#     word = sys.argv[1]
     
-else:
-    sys.exit('input word')
+# else:
+#     sys.exit('input word.')
 
 
 conn = sqlite3.connect('engdic.sqlite')
 c = conn.cursor()
 
-with open('ws2.txt') as f:
-    data = [line.rstrip() for line in f.readlines()]
+with open('word_list_school1_test.csv', newline='') as csvfile:
+    content = csv.reader(csvfile, delimiter=',', quotechar='"')
+    data = list(content)
+    # for row in data:
+    #     print(', '.join(row))
+
+
+for d in data[:100]:
+    word = d[0]
+    c.execute('SELECT word,wordtype,definition FROM entries WHERE word=? collate nocase', (word,))
+    wordtype = c.fetchone()
+    if wordtype == None:
+        d.append('')
+    else:
+        d.append(wordtype[1])
+
+    c.execute('SELECT word,wordtype,count(*) FROM entries WHERE (lower(word) LIKE ? ) group by wordtype ', (word,))
+    wordtype = c.fetchone()
+    if wordtype == None:
+        d.append('')
+    else:
+        d.append(wordtype[1])
+
+with open('word_list_school1_test_result.csv', 'w', newline='') as myfile:
+     writer = csv.writer(myfile, quoting=csv.QUOTE_MINIMAL)
+     for line in data:
+            writer.writerow(line)
+    #  writer.writerow(data)
 
 # for row in c.execute('SELECT word,wordtype,definition FROM entries WHERE word=? collate nocase', (word,)):
 #     print(row)
 
-c.execute('SELECT word,wordtype,definition FROM entries WHERE word=? collate nocase', (word,))
-print(c.fetchone()[1])
+# c.execute('SELECT word,wordtype,definition FROM entries WHERE word=? collate nocase', (word,))
+# print(c.fetchone()[1])
 
 conn.commit()   
 conn.close()
